6932284aab276e71b45fcdff26eaf3b3e3cc7fda
Smith <andras@sexybuddha.hu>
2017-07-03 00:04:13 +0200

    cleanup + add electron-unhandled
     * spaces > tabs
     * cleanup stylesheet

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -5,46 +5,47 @@ const app = electron.app;
 
 // Adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
+require('electron-unhandled')();
 
 // Prevent window being garbage collected
 let mainWindow;
 
 function onClosed() {
-	// Dereference the window
-	// For multiple windows store them in an array
-	mainWindow = null;
+    // Dereference the window
+    // For multiple windows store them in an array
+    mainWindow = null;
 }
 
 function createMainWindow() {
-	const win = new electron.BrowserWindow({
-		width: 600,
-		height: 400
-	});
+    const win = new electron.BrowserWindow({
+        width: 600,
+        height: 400
+    });
 
-	const url = require('url').format({
-		protocol: 'file',
-		slashes: true,
-		pathname: require('path').join(__dirname, 'index.html')
-	});
+    const url = require('url').format({
+        protocol: 'file',
+        slashes: true,
+        pathname: require('path').join(__dirname, 'index.html')
+    });
 
-	win.loadURL(url);
-	win.on('closed', onClosed);
+    win.loadURL(url);
+    win.on('closed', onClosed);
 
-	return win;
+    return win;
 }
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-	if (!mainWindow) {
-		mainWindow = createMainWindow();
-	}
+    if (!mainWindow) {
+        mainWindow = createMainWindow();
+    }
 });
 
 app.on('ready', () => {
-	mainWindow = createMainWindow();
+    mainWindow = createMainWindow();
 });
